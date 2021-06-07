ee9895635b9923df709ac1b5d8117a5f78845fa1
abildgaard06 <abildgaard06@hotmail.com>
2021-06-07 11:05:43 +0200

    Nu med singleprod.php og adgang til den.

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ include('functions.php');
 connect();
 include('header.php');
 
-if(isset($_GET['p'])){
+if(isset($_GET['a'])){
+  include('templates/singleProd.php');
+}elseif(isset($_GET['p'])){
   $pid = $_GET['p'];
   $page = getPage($pid);
   include('templates/' . $page[0]['template']);
